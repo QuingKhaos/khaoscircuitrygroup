b936be6c874066f7084dc3f9fb8c18cf7341ad5c
Em Karisch <emii.khaos@gmail.com>
2026-07-25 15:09:26 +0200

    refactor: move updating base game combinators into data-updates phase

---
+++ data-updates.lua
@@ -0,0 +1,10 @@
+local lib = require("__khaoscircuitrygroup__.prototypes.lib")
+
+lib.update_subgroup("power-switch", "power-switch", "circuit-connection")
+lib.update_subgroup("arithmetic-combinator", "arithmetic-combinator", "circuit-combinator-arithmetic")
+lib.update_subgroup("decider-combinator", "decider-combinator", "circuit-combinator-decider")
+lib.update_subgroup("selector-combinator", "selector-combinator", "circuit-combinator-selector")
+lib.update_subgroup("constant-combinator", "constant-combinator", "circuit-combinator-constant")
+lib.update_subgroup("display-panel", "display-panel", "circuit-visual")
+lib.update_subgroup("lamp", "small-lamp", "circuit-visual-lamp")
+lib.update_subgroup("programmable-speaker", "programmable-speaker", "circuit-auditory")
--- data.lua
+++ data.lua
@@ -1,6 +1,3 @@
-local khaoslib_entity = require("__khaoslib__.prototypes.entity")
-local khaoslib_item = require("__khaoslib__.prototypes.item")
-local khaoslib_recipe = require("__khaoslib__.prototypes.recipe")
 local khaoslib_item_group = require("__khaoslib__.prototypes.item-group")
 local khaoslib_item_subgroup = require("__khaoslib__.prototypes.item-subgroup")
 
@@ -28,25 +25,3 @@ khaoslib_item_subgroup:load {type = "item-subgroup", name = "circuit-input", gro
 khaoslib_item_subgroup:load {type = "item-subgroup", name = "circuit-visual", group = "circuitry", order = "e[circuit-visual]"} :commit()
 khaoslib_item_subgroup:load {type = "item-subgroup", name = "circuit-visual-lamp", group = "circuitry", order = "e[circuit-visual]-a[lamp]"} :commit()
 khaoslib_item_subgroup:load {type = "item-subgroup", name = "circuit-auditory", group = "circuitry", order = "f[circuit-auditory]"} :commit()
-
---- @param type string
---- @param name string
---- @param subgroup data.ItemSubGroupID
-local function update_subgroup(type, name, subgroup)
-  if not settings.startup["khaoscircuitrygroup-individual-combinator-subgroups"].value then
-    subgroup = subgroup:gsub("%-arithmetic", ""):gsub("%-decider", ""):gsub("%-selector", ""):gsub("%-constant", ""):gsub("%-lamp", "")
-  end
-
-  khaoslib_entity:load(type, name):set {subgroup = subgroup} :commit()
-  khaoslib_item:load(name):set {subgroup = subgroup} :commit()
-  khaoslib_recipe:load(name):set {subgroup = subgroup} :commit()
-end
-
-update_subgroup("power-switch", "power-switch", "circuit-connection")
-update_subgroup("arithmetic-combinator", "arithmetic-combinator", "circuit-combinator-arithmetic")
-update_subgroup("decider-combinator", "decider-combinator", "circuit-combinator-decider")
-update_subgroup("selector-combinator", "selector-combinator", "circuit-combinator-selector")
-update_subgroup("constant-combinator", "constant-combinator", "circuit-combinator-constant")
-update_subgroup("display-panel", "display-panel", "circuit-visual")
-update_subgroup("lamp", "small-lamp", "circuit-visual-lamp")
-update_subgroup("programmable-speaker", "programmable-speaker", "circuit-auditory")
