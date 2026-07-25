c120611cdd358574042faf1cd2e363a9ca8a0bc8
Em Karisch <emii.khaos@gmail.com>
2026-07-25 14:50:28 +0200

    chore: updates from mod template

--- .dev/factorio_2.1.ps1
+++ .dev/factorio_2.1.ps1
@@ -2,12 +2,13 @@ Set-Variable -Name workspace -Value $Env:FACTORIO_WORKSPACE
 Set-Variable -Name mod_base -Value $Env:FACTORIO_21_MODS_BASE
 Set-Variable -Name package -Value "khaoscircuitrygroup"
 
-New-Item -ItemType Directory -Force -Path "$mod_base\$package" | Out-Null
+New-Item -ItemType Directory -Force -Path "$mod_base" | Out-Null
+Copy-Item -Force -Recurse -Path "$workspace\$package\.dev\mods2.1\*" -Destination "$mod_base" -ErrorAction SilentlyContinue
 
 Set-Variable -Name version -Value (Get-Content "$workspace\$package\info.json" | ConvertFrom-Json).version
-Move-Item -Path "$workspace\$package\${package}_${version}.zip" -Destination "${mod_base}\${package}" -Force -ErrorAction SilentlyContinue
+Move-Item -Path "$workspace\$package\${package}_${version}.zip" -Destination "$mod_base" -Force -ErrorAction SilentlyContinue
 
 Set-Variable -Name version_khaoslib -Value (Get-Content "$workspace\khaoslib\info.json" | ConvertFrom-Json).version
-Move-Item -Path "$workspace\khaoslib\khaoslib_$version_khaoslib.zip" -Destination "${mod_base}\${package}" -Force -ErrorAction SilentlyContinue
+Move-Item -Path "$workspace\khaoslib\khaoslib_$version_khaoslib.zip" -Destination "$mod_base" -Force -ErrorAction SilentlyContinue
 
-Start-Process -FilePath "${Env:FACTORIO_21_PATH}\bin\x64\Factorio.exe" -ArgumentList "--mod-directory ${mod_base}\${package}"
+Start-Process -FilePath "${Env:FACTORIO_21_PATH}\bin\x64\Factorio.exe" -ArgumentList "--mod-directory $mod_base"
